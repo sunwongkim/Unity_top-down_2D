ef00cf16bb583b9601df13610569c133a4973836
sunwongkim <sunwongkim0609@gmail.com>
2025-01-18 19:02:02 +0900

    import Hierarchy Designer, 애니메이션 로직, 타일맵 오류 해결

---
+++ Assets/Player.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    Rigidbody2D rb;
+    Animator animator;
+    Vector2 movement;
+    int direction; // 애니메이션 방향 (상:0, 우:1, 하:2, 좌:3)
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal"); // A, D
+        float vertical = Input.GetAxisRaw("Vertical");     // W, S
+
+        movement = new Vector2(horizontal, vertical);
+
+        // 방향 계산 (대각선 이동 시 수평 애니메이션 우선)
+        if (movement.x != 0) { // 수평 우선(필터링)
+            direction = (movement.x > 0) ? 1 : 3; // 우(1), 좌(3)
+        } else if (movement.y != 0) {
+            direction = (movement.y > 0) ? 0 : 2; // 상(0), 하(2)
+        }
+
+        // animator.SetInteger("Direction", direction);
+        // animator.SetBool("IsMoving", movement != Vector2.zero);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement.normalized * moveSpeed;
+    }
+}
